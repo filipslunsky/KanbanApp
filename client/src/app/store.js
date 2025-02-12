9f1f3ac6c98efaad5dddc21750efdec8fc8bd29b
filipslunsky <fslunsky@gmail.com>
2025-02-12 15:32:32 +0100

    added redux store to /client

---
+++ client/src/app/store.js
@@ -0,0 +1,12 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+// import userSlice from '../features/user/state/slice.js';
+
+const appReducer = combineReducers({
+//    user: userSlice,
+});
+
+const store = configureStore({
+    reducer: appReducer
+});
+
+export default store;
