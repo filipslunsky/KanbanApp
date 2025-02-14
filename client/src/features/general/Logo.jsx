017f2d8c107398eca74efd24a8c10c27c3b8d0f0
filipslunsky <fslunsky@gmail.com>
2025-02-14 17:50:15 +0100

    logos imported to Logo.jsx

--- client/src/features/general/Logo.jsx
+++ client/src/features/general/Logo.jsx
@@ -1,3 +1,9 @@
+import { useSelector } from 'react-redux';
+import logoLight from '../../assets/img/logo-light.svg';
+import logoDark from '../../assets/img/logo-dark.svg';
+import logoMobile from '../../assets/img/logo-mobile.svg';
+import './logo.css';
+
 const Logo = () => {
     return (
         <>
