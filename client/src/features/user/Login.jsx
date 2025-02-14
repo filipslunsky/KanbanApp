04ec1206d510381849a19ffa48df46a903bc3e00
filipslunsky <fslunsky@gmail.com>
2025-02-14 17:07:17 +0100

    Home.jsx elements added

--- client/src/features/user/Login.jsx
+++ client/src/features/user/Login.jsx
@@ -1,9 +1,73 @@
-const Login = () => {
+import { useRef, useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { loginUser } from './state/slice.js';
+
+const LoginUser = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const loggedIn = useSelector(state => state.user.loggedIn);
+
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    useEffect(() => {
+        if (loggedIn) {
+            navigate('/links');
+        }
+    }, [loggedIn, navigate]);
+
+
+    const handleLogin = () => {
+        if (emailRef.current.value.length === 0 || passwordRef.current.value.length === 0) return;
+
+        const userInfo = {
+            email: emailRef.current.value,
+            password: passwordRef.current.value
+        };
+
+        dispatch(loginUser(userInfo));
+    };
+
+    const handleCancel = () => {
+        navigate('/');
+    };
+
     return (
         <>
-            <h2>Login</h2>
+            <div className="userFormContainer">
+                <img src={logoImage} alt="logo" className="userFormLogoImage" />
+                <h2 className="userFormName">Log In</h2>
+                <div className="userInputContainer">
+                    <input
+                    type="email"
+                    className='userInputField'
+                    ref={emailRef}
+                    placeholder='email address'
+                    required
+                    />
+                </div>
+                <div className="userInputContainer">
+                    <input
+                    type="password"
+                    className='userInputField'
+                    ref={passwordRef}
+                    placeholder='password'
+                    required
+                    />
+                </div>
+                <div className="userControlsContainer">
+                    <button className='userConfirmButton' onClick={handleLogin}>Login</button>
+                    <button className="userCancelButton" onClick={handleCancel}>Cancel</button>
+                </div>
+                <div className="userStatusContainer">
+                    <p className='userStatusMessage'>You don't have an account yet?</p>
+                    <Link to='/user/register' className='formLink'>Create a New Account</Link>
+                </div>
+            </div>
         </>
     );
 }
  
-export default Login;
+export default LoginUser;
