292fb9d53f4b9709cceae0c82e3577cf2d6985fe
filipslunsky <fslunsky@gmail.com>
2025-02-14 16:50:14 +0100

    deleteSubtask endpoint added

--- models/subtasksModel.js
+++ models/subtasksModel.js
@@ -24,17 +24,70 @@ const _addSubtask = async (subtaskName, taskId) => {
                 message: 'Task successfully created',
                 subtask: newSubtask[0],
             }
-        })
+        });
         
     } catch (error) {
         console.error('Transaction error:', error);
-        return { success: false, message: `Error adding task: ${error.message}` };
+        return { success: false, message: `Error adding subtask: ${error.message}` };
     }
 };
 
-const _updateSubtaskById = async () => {};
+const _updateSubtaskById = async (subtaskId, subtaskName, isCompleted) => {
+    try {
+        return await db.transaction(async (trx) => {
+            const subtask = await trx('subtasks')
+            .select('subtask_id')
+            .where({subtask_id: subtaskId})
+            .first();
 
-const _deleteSubtaskById = async () => {};
+            if (!subtask) {
+                return { success: false, message: 'Subtask not found' }
+            };
+
+            const updatedSubtask = await trx('subtasks')
+            .update({
+                subtask_name: subtaskName,
+                is_completed: isCompleted,
+            })
+            .where({subtask_id: subtask.subtask_id})
+            .returning(['subtask_id', 'subtask_name', 'is_completed', 'task_id']);
+
+            return {
+                success: true,
+                message: 'Subtask successfully updated',
+                subtask: updatedSubtask[0]
+            };
+        });
+    } catch (error) {
+        console.error('Transaction error:', error);
+        return { success: false, message: `Error updating subtask: ${error.message}`};
+    }
+};
+
+const _deleteSubtaskById = async (subtaskId) => {
+    try {
+        return await db.transaction(async (trx) => {
+            const subtask = await trx('subtasks')
+            .select('subtask_id')
+            .where({subtask_id: subtaskId})
+            .first();
+
+            if (!subtask) {
+                return { success: false, message: 'Subtask not found' }
+            };
+
+            await trx('subtasks')
+            .delete()
+            .where({subtask_id: subtask.subtask_id});
+
+            return { success: true, message: 'Subtask successfully deleted' };
+        });
+        
+    } catch (error) {
+        console.error('Transaction error:', error);
+        return { success: false, message: `Error deleting subtask: ${error.message}`};
+    }
+};
 
 module.exports = {
     _addSubtask,
