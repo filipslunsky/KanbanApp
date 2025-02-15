be7ace95e9a95919453b275d838dd4f392aa5018
filipslunsky <fslunsky@gmail.com>
2025-02-15 10:01:12 +0100

    SideBar.jsx funcs added

--- client/src/features/general/Boards.jsx
+++ client/src/features/general/Boards.jsx
@@ -1,11 +1,15 @@
+import { useSelector } from "react-redux";
 import Sidebar from "./Sidebar";
 import './boards.css';
 
 const Boards = () => {
+    const sideBar = useSelector(state => state.general.sideBar);
+    const nightMode = useSelector(state => state.general.nightMode);
+
     return (
         <>
-            <div className="boardsMainContainer">
-                <Sidebar />
+            <div className={nightMode ? "boardsMainContainer nightMode" : "boardsMainContainer"}>
+                {sideBar && <Sidebar />}
                 <div className="boardsRightContainer"></div>
             </div>
         </>
--- client/src/features/general/Sidebar.jsx
+++ client/src/features/general/Sidebar.jsx
@@ -1,14 +1,46 @@
 import { useSelector, useDispatch } from "react-redux";
+import { toggleNightMode, toggleSideBar } from "./state/slice";
 import Logo from "./Logo";
 import Projects from "./Projects";
+import lightThemeIcon from '../../assets/img/icon-light-theme.svg';
+import darkThemeIcon from '../../assets/img/icon-dark-theme.svg';
+import hideSideBarIcon from '../../assets/img/icon-hide-sidebar.svg';
 import './sidebar.css';
 
 const Sidebar = () => {
+    // global variables
+    const dispatch = useDispatch();
+    const nightMode = useSelector(state => state.general.nightMode);
+
+    const handleToggleNightMode = () => {
+        dispatch(toggleNightMode());
+    };
+
+    const handleToggleSideBar = () => {
+        dispatch(toggleSideBar());
+    };
+
     return (
         <>
-            <div className="sidebarMainContainer">
+            <div className={nightMode ? "sidebarMainContainer nightMode" : "sidebarMainContainer"}>
                 <Logo />
                 <Projects />
+                <div className="sidebarControlsContainer">
+                    <div className="nightModeSwitchContainer">
+                        <img src={lightThemeIcon} alt="icon" className="nightModeIcon" />
+                        <label className="switch">
+                            <input
+                                type="checkbox"
+                                onChange={handleToggleNightMode}
+                            />
+                            <span className="slider"></span>
+                        </label>
+                        <img src={darkThemeIcon} alt="icon" className="nightModeIcon" />
+                    </div>
+                    <div className="hideSideBarContainer">
+                        <button className="hideSidebarButton" onClick={handleToggleSideBar}><img src={hideSideBarIcon} alt="icon" className="hideSideBarIcon" />Hide Sidebar</button>
+                    </div>
+                </div>
             </div>
         </>
     );
