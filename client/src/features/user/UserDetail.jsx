674d6aaf04f8642c2ba59db2a832ffe980408f76
filipslunsky <fslunsky@gmail.com>
2025-02-20 17:48:44 +0100

    added styling to UserDetail.jsx

--- client/src/features/user/UserDetail.jsx
+++ client/src/features/user/UserDetail.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { toggleProfileDetailWindow } from '../general/state/slice.js';
